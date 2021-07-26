8299b8e62d276f875e1c394f6ff2978ee10e2d6f
boybenson <ybenson96@gmail.com>
2021-07-26 08:15:14 +0000

    some little practice on javascript.info

---
+++ server.js
@@ -0,0 +1,19 @@
+// var passed = 5;
+
+// const addTo = function () {
+//   let value = 3;
+//   return passed + 3;
+// };
+
+// console.log(addTo());
+
+// var passed = 6;
+
+// console.log(addTo());
+test();
+
+function test() {
+  a = 78;
+}
+
+console.log(a);
